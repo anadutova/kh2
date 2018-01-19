86ab828abf091987f3404fc3ac189c51a801d63b
anadutova <d.anasztazia@gmail.com>
2018-01-19 22:13:56 +0100

    Tablazat feltoltese JS-bol

---
+++ script.js
@@ -0,0 +1,50 @@
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+
+function onReady(){
+    var lectures = new Array(5).fill(
+        {
+            date: "12.25",
+            topic: "Bemutatkozás, játékszabályok",
+            address:  null,
+            lecturer: "NK",
+            comment:  "Lorem ipsum dolor sit amet"     
+        }
+    );
+
+    var table = document.getElementById("table-body");
+
+    for (var lecture of lectures){
+        var row = document.createElement("tr");
+        row.setAttribute("class", "table__row");
+
+        for(let i = 0; i < 5; i++) {
+            var cell = document.createElement("td");
+            if (i == 0 || i == 2 || i == 3) cell.setAttribute("class", "table__cell--text-align-center");
+            var cellText;
+            switch (i) {
+                case 0: cellText = document.createTextNode(lecture.date);
+                break;
+                case 1: cellText = document.createTextNode(lecture.topic);
+                break;
+                case 2: cellText = document.createTextNode(lecture.address);
+                break;
+                case 3: cellText = document.createTextNode(lecture.lecturer);
+                break;
+                case 4: cellText = document.createTextNode(lecture.comment);
+                break;             
+            }
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
+}
+
+ready(onReady);
