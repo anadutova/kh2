bda6996624d2b868edcb76b71d68b198278f06e7
anadutova <d.anasztazia@gmail.com>
2018-01-28 22:15:55 +0100

    Responsive table

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ function onReady(){
         }
     );
 
+    lectures.push( {
+        date: "12.25",
+            topic: "Bemutatkozás, játékszabályok",
+            address:  null,
+            lecturer: "NK",
+            comment:  "Lorem ipsum dolor sit amet \u25b6 \n Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet" 
+    });
+
     var table = document.getElementById("table-body");
 
     for (var lecture of lectures){
@@ -26,7 +34,7 @@ function onReady(){
         let cell = document.createElement("td");
         //unicode for BLACK RIGHT-POINTING TRIANGLE
         cell.textContent = "\u25b6";
-        cell.classList.add("class", "table__cell--text-align-center", "table__cell")
+        cell.classList.add("class", "table__cell--text-align-center", "table__cell--xs-only")
         cell.addEventListener("click", function(event){
             var row = event.target.parentElement;
             var cells = row.querySelectorAll(".js-collapsibleCell");
@@ -40,7 +48,7 @@ function onReady(){
             let cell = document.createElement("td");
             if (i == 0 || i == 2 || i == 2) cell.setAttribute("class", "table__cell--text-align-center  table__cell--inflexible");
             else cell.setAttribute("class", "table__cell--flexible");
-            if (i == 3) cell.classList.add("table_cell--data-lecturer", "js-collapsibleCell");
+            if (i == 3) cell.classList.add("table_cell--data-lecturer", "js-collapsibleCell", "table__cell--text-align-center");
             if (i == 4) cell.classList.add("table_cell--data-comment", "js-collapsibleCell");
             cell.classList.add("table__cell");
             let cellText;
@@ -53,10 +61,10 @@ function onReady(){
                 break;
                 case 3: cellText = document.createTextNode(lecture.lecturer);
                 break;
-                case 4: cellText = document.createTextNode(lecture.comment);
+                case 4: cell.textContent = lecture.comment;
                 break;             
             }
-            cell.appendChild(cellText);
+            if (i !== 4) cell.appendChild(cellText);
             row.appendChild(cell);
 
             if (i == 2){
